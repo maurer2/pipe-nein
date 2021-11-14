8faffc577f013658e78f9afd18bab4820c040caa
Martin <mr.mmaurer+work@gmail.com>
2021-11-14 19:04:10 +0000

    Added output variable from script

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const emoji = require('node-emoji')
 
 const emojified = emoji.emojify(':cat:');
-console.log(`hello ${emojified}`)
+// console.log(`hello ${emojified}`)
+
+const packageLockVersion = 1;
+
+// console.log(`hello ${emojified}`)
+
+console.log(packageLockVersion)
+
+// process.stdout.write(1);
+process.exitCode = 0;
